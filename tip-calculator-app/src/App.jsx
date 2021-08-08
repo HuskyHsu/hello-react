5fe147ed42c02280d962500b61cd31226644b572
Shihchi Hsu <Shihchi.Hsu@dlinkcorp.com>
2021-08-08 23:31:27 +0800

    init

---
+++ tip-calculator-app/src/App.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { ReactComponent as Logo } from "./images/logo.svg";
+import { ReactComponent as Dollar } from "./images/icon-dollar.svg";
+
+function clsx(...className) {
+    return className.filter(Boolean).join(" ");
+}
+
+function Header() {
+    return (
+        <header className="w-20 mt-16 mb-8">
+            <Logo />
+        </header>
+    );
+}
+
+function Card({ children, className }) {
+    return <div className={clsx("p-6 shadow w-full", className)}>{children}</div>;
+}
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <main className="h-full w-full flex flex-col items-center">
+            {/* header */}
+            <Header />
+
+            <Card className="bg-white rounded-3xl">
+                <form>
+                    <label className="text-text-dark text-sm" htmlFor="bill">
+                        Bill
+                    </label>
+                    <input
+                        className="w-full p-2 bg-lightest text-right font-bold text-darkest"
+                        id="bill"
+                        type="number"
+                        value="142.55"
+                        step="0.01"
+                    />
+                    <Dollar />
+                </form>
+                {/* select tip */}
+                {/* number of people */}
+                {/* result */}
+            </Card>
+        </main>
+    );
+}
+
+export default App;
---
+++ tip-calculator-app/tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    mode: "jit",
+    purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                DEFAULT: "hsl(172, 67%, 45%)",
+                "text-dark": "hsl(186, 14%, 43%)",
+                light: "hsl(185, 41%, 84%)",
+                darkest: "hsl(183, 100%, 15%)",
+                lightest: "hsl(189, 41%, 97%)",
+            },
+            fontFamily: {
+                spaceMono: ["Space Mono", "monospace", "sans-serif"],
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
