9fda674d261e3d061b1c833f7c0f752a36a4f4b7
HuskyHsu <schi.hsu@gmail.com>
2021-08-12 22:49:11 +0800

    add Select Tip RadioGroup

--- tip-calculator-app/src/App.jsx
+++ tip-calculator-app/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, cloneElement } from "react";
+import React, { cloneElement, Children } from "react";
 import { Icon } from "./components";
 import clsx from "clsx";
 
@@ -19,7 +19,7 @@ function Card({ children, className }) {
 function TextField({ id, label, icon }) {
   return (
     <div>
-      <label className="text-text-dark text-sm font-bold" htmlFor={id}>
+      <label className="text-text-dark text-sm font-bold pb-2" htmlFor={id}>
         {label}
       </label>
       <div className="relative flex items-center">
@@ -30,6 +30,41 @@ function TextField({ id, label, icon }) {
         />
         {cloneElement(icon, { className: "h-4 w-4 absolute m-4" })}
       </div>
+    </div>
+  );
+}
+
+function RadioGroup({ name, label, children }) {
+  return (
+    <fieldset>
+      <legend className="text-text-dark text-sm font-bold pb-2">{label}</legend>
+      <div className="grid grid-cols-2 gap-4">
+        {Children.map(children, (child) => cloneElement(child, { name: name }))}
+      </div>
+    </fieldset>
+  );
+}
+
+function Radio({ value, label, name }) {
+  return (
+    <div>
+      <input
+        type="radio"
+        name={name}
+        id={value}
+        value={value}
+        className="sr-only peer"
+      />
+      <label
+        htmlFor={value}
+        className={clsx(
+          "block bg-darkest py-4 w-full rounded-md",
+          "text-center text-white text-xl font-bold",
+          "peer-checked:bg-light peer-checked:text-darkest"
+        )}
+      >
+        {label}
+      </label>
     </div>
   );
 }
@@ -45,6 +80,15 @@ function App() {
             <TextField id="bill" label="Bill" icon={<Icon.Dollar />} />
           </form>
           {/* select tip */}
+          <RadioGroup name="tips" label="Select Tip %">
+            <Radio value="5" label="5%" />
+            <Radio value="10" label="10%" />
+            <Radio value="15" label="15%" />
+            <Radio value="25" label="25%" />
+            <Radio value="50" label="50%" />
+            <Radio value="custom" label="Custom" />
+          </RadioGroup>
+
           {/* number of people */}
           <TextField
             id="people"
--- tip-calculator-app/tailwind.config.js
+++ tip-calculator-app/tailwind.config.js
@@ -1,23 +1,24 @@
 module.exports = {
-    mode: "jit",
-    purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {
-            colors: {
-                DEFAULT: "hsl(172, 67%, 45%)",
-                "text-dark": "hsl(186, 14%, 43%)",
-                light: "hsl(185, 41%, 84%)",
-                darkest: "hsl(183, 100%, 15%)",
-                lightest: "hsl(189, 41%, 97%)",
-            },
-            fontFamily: {
-                spaceMono: ["Space Mono", "monospace", "sans-serif"],
-            },
-        },
+  mode: "jit",
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        DEFAULT: "hsl(172, 67%, 45%)",
+        "text-dark": "hsl(186, 14%, 43%)",
+        light: "hsl(185, 41%, 84%)",
+        darkest: "hsl(183, 100%, 15%)",
+        lightest: "hsl(189, 41%, 97%)",
+        light: "hsl(172, 67%, 45%)",
+      },
+      fontFamily: {
+        spaceMono: ["Space Mono", "monospace", "sans-serif"],
+      },
     },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
